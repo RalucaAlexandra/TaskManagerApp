5ea7df05101e8882f62907e7e75f7e84cd4efdee
RalucaAlexandra <48680130+RalucaAlexandra@users.noreply.github.com>
2021-05-04 23:11:51 +0300

    Identity and role management

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -26,6 +26,8 @@ namespace API.Controllers
             _mapper = mapper;
             _userRepository = userRepository;
         }
+
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
@@ -42,6 +44,7 @@ namespace API.Controllers
 
             return Ok(users);
         }
+
 
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
